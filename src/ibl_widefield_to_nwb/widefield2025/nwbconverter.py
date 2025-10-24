79d05c47e4ea1eec9e8bfce19685b730bf75d671
weiglszonja <wszonja@gmail.com>
2025-10-24 12:05:54 +0200

    rename converter

--- src/ibl_widefield_to_nwb/widefield2025/nwbconverter.py
+++ src/ibl_widefield_to_nwb/widefield2025/nwbconverter.py
@@ -1,15 +1,14 @@
 """Primary NWBConverter class for this dataset."""
+
 from neuroconv import NWBConverter
 
-from ibl_widefield_to_nwb.widefield2025 import WidefieldSegmentationInterface
+from .ibl_widefield_segmentationinterface import WidefieldSegmentationInterface
 
 
-class Widefield2025NWBConverter(NWBConverter):
-    """Primary conversion class for Widefield data."""
+class WidefieldProcessedNWBConverter(NWBConverter):
+    """Primary conversion class for Widefield processed data."""
 
     data_interface_classes = dict(
-        #ImagingBlue=WidefieldImagingInterface,
-        #ImagingViolet=WidefieldImagingInterface,
         SegmentationBlue=WidefieldSegmentationInterface,
         SegmentationViolet=WidefieldSegmentationInterface,
     )
