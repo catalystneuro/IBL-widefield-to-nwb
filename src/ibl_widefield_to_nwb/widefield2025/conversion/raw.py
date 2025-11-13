39d7b4fcd4f4d6a1cc0fe7dbbc8c25cc4c5438d8
weiglszonja <wszonja@gmail.com>
2025-11-13 16:34:37 +0100

    remove time alignment part temporarily

--- src/ibl_widefield_to_nwb/widefield2025/conversion/raw.py
+++ src/ibl_widefield_to_nwb/widefield2025/conversion/raw.py
@@ -5,8 +5,6 @@ import time
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
-import numpy as np
-import pandas as pd
 from neuroconv.utils import dict_deep_update, load_dict_from_file
 from pynwb import read_nwb
 
@@ -14,62 +12,10 @@ from ibl_widefield_to_nwb.widefield2025 import WidefieldRawNWBConverter
 from ibl_widefield_to_nwb.widefield2025.datainterfaces import WidefieldImagingInterface
 
 
-def _get_channel_id_from_wavelength(
-    excitation_wavelength_nm: int,
-    light_source_properties_file_path: Path,
-) -> int:
-    """
-    Get the channel ID corresponding to a specific wavelength.
-    Parameters
-    ----------
-    excitation_wavelength_nm : int
-        The excitation wavelength in nanometers.
-    light_source_properties_file_path : Path
-        Path to the .htsv file containing light source properties.
-    Returns
-    -------
-    int
-        The channel ID corresponding to the specified wavelength.
-    """
-    light_sources = pd.read_csv(light_source_properties_file_path)
-    channel_id = light_sources.loc[light_sources["wavelength"] == excitation_wavelength_nm, "channel_id"].tolist()
-    if len(channel_id) == 0:
-        raise ValueError(f"No channel ID found for wavelength {excitation_wavelength_nm} nm.")
-    return channel_id[0]
-
-
-def _get_imaging_times_by_channel_id(
-    aligned_times_file_path: Path,
-    light_source_file_path: Path,
-    channel_id: int,
-) -> np.ndarray:
-    """
-    Get imaging times for a specific channel ID.
-    Parameters
-    ----------
-    aligned_times_file_path : Path
-        Path to the .npy file containing aligned imaging times.
-    light_source_file_path : Path
-        Path to the .npy file containing light source channel IDs.
-    channel_id : int
-        The channel ID for which to retrieve imaging times.
-    Returns
-    -------
-    np.ndarray
-        Array of imaging times corresponding to the specified channel ID.
-    """
-    all_times = np.load(aligned_times_file_path)
-    light_sources = np.load(light_source_file_path)
-
-    times_per_channel_id = all_times[light_sources == channel_id]
-    return times_per_channel_id
-
-
 def convert_raw_session(
     nwbfile_path: str | Path,
     raw_data_dir_path: str | Path,
     cache_dir_path: str | Path,
-    processed_data_dir_path: str | Path,
     functional_wavelength_nm: int,
     isosbestic_wavelength_nm: int,
     force_cache: bool = False,
@@ -184,36 +130,6 @@ def convert_raw_session(
     # ========================================================================
 
     converter = WidefieldRawNWBConverter(data_interfaces=data_interfaces)
-
-    # ========================================================================
-    # STEP 4: Align times
-    # ========================================================================
-
-    # Get imaging times and add to conversion options
-    aligned_times_file_path = processed_data_dir_path / "imaging.times.npy"
-    light_source_file_path = processed_data_dir_path / "imaging.imagingLightSource.npy"
-    light_source_properties_file_path = processed_data_dir_path / "imagingLightSource.properties.htsv"
-    functional_channel_id = _get_channel_id_from_wavelength(
-        excitation_wavelength_nm=functional_wavelength_nm,
-        light_source_properties_file_path=light_source_properties_file_path,
-    )
-    functional_imaging_times = _get_imaging_times_by_channel_id(
-        aligned_times_file_path=aligned_times_file_path,
-        light_source_file_path=light_source_file_path,
-        channel_id=functional_channel_id,
-    )
-    converter.data_interface_objects["ImagingBlue"].imaging_extractor.set_times(times=functional_imaging_times)
-
-    isosbestic_channel_id = _get_channel_id_from_wavelength(
-        excitation_wavelength_nm=isosbestic_wavelength_nm,
-        light_source_properties_file_path=light_source_properties_file_path,
-    )
-    isosbestic_imaging_times = _get_imaging_times_by_channel_id(
-        aligned_times_file_path=aligned_times_file_path,
-        light_source_file_path=light_source_file_path,
-        channel_id=isosbestic_channel_id,
-    )
-    converter.data_interface_objects["ImagingViolet"].imaging_extractor.set_times(times=isosbestic_imaging_times)
 
     # ========================================================================
     # STEP 5: Get metadata
