fca723af6d5be809567b053109677554c69a7196
weiglszonja <wszonja@gmail.com>
2026-01-08 18:04:09 +0100

    feat: add function to retrieve raw behavior data interfaces

--- src/ibl_widefield_to_nwb/widefield2025/conversion/__init__.py
+++ src/ibl_widefield_to_nwb/widefield2025/conversion/__init__.py
@@ -1,4 +1,4 @@
-from .behavior import get_processed_behavior_interfaces
+from .behavior import get_processed_behavior_interfaces, get_raw_behavior_interfaces
 from .build_cache import build_frame_cache, validate_cache
 from .processed import convert_processed_session
 from .raw import convert_raw_session
@@ -9,4 +9,5 @@ __all__ = [
     "convert_raw_session",
     "convert_processed_session",
     "get_processed_behavior_interfaces",
+    "get_raw_behavior_interfaces",
 ]
