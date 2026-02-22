4f7df1baa7b9c661e924ccc7f4b24f029546d1fc
weiglszonja <wszonja@gmail.com>
2026-02-22 19:45:03 +0100

    feat: add IblWidefieldDAQInterface import to module

--- src/ibl_widefield_to_nwb/widefield2025/datainterfaces/__init__.py
+++ src/ibl_widefield_to_nwb/widefield2025/datainterfaces/__init__.py
@@ -1,4 +1,5 @@
 from ._ibl_nidq_interface import IblNIDQInterface
+from ._ibl_widefield_DAQ_interface import IblWidefieldDAQInterface
 from ._ibl_widefield_imaginginterface import WidefieldImagingInterface
 from ._ibl_widefield_landmarksinterface import IblWidefieldLandmarksInterface
 from ._ibl_widefield_SVDinterface import WidefieldSVDInterface
@@ -7,5 +8,6 @@ __all__ = [
     "WidefieldImagingInterface",
     "WidefieldSVDInterface",
     "IblNIDQInterface",
+    "IblWidefieldDAQInterface",
     "IblWidefieldLandmarksInterface",
 ]
